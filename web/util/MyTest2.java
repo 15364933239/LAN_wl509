6aed8de89d615f3f6c7df61080179d44a7b84204
PeiTong Li <45646492+15364933239@users.noreply.github.com>
2019-03-22 12:04:10 +0800

    Update and rename MyTest2.java to web/util/MyTest2.java

--- web/util/MyTest2.java
+++ web/util/MyTest2.java
@@ -32,6 +32,9 @@ public class MyTest2 {
 	 * @throws IOException
 	 * @throws SAXException
 	 */
+//	public static void main(String[] args) throws InterruptedException, IOException, SAXException {
+//		getOnLineDevices("123456");
+//	}
 	public static List<Device> getOnLineDevices(String password) throws InterruptedException, IOException, SAXException {
 		
 		//使用System.setProperty方法设置对应的系统属性
@@ -72,24 +75,24 @@ public class MyTest2 {
 		if (trList1.size() < 10) {
 			List<WebElement> trList = table.findElements(By.xpath("//tbody/tr[@style='display: table-row;']"));
 			System.out.println("共有"+(trList1.size()-1)+"台设备在线");
-
+			int i = 0;
 			for (WebElement tr : trList) {
+				i++;
+				if(i==1) continue;
 				System.out.println();
 				List<WebElement> tdList = tr.findElements(By.tagName("td"));
 				Device device = new Device();
 				String name = "";
-				if (tdList.get(0) != null) {
-					name = tdList.get(0).getAttribute("title");
-					System.out.println("一页的设备名字:"+name);
-				}
+				name = tdList.get(0).getAttribute("title");
+				System.out.println("一页的设备名字:"+name);
 				device.setDname(name);
 
 				String mac = "";
-				if (tdList.get(1) != null) {
-					mac = tdList.get(1).getAttribute("title");
-					System.out.println("一页的马克地址："+mac);
-				}
+				mac = tdList.get(1).getAttribute("title");
+				System.out.println("一页的马克地址："+mac);
 				device.setMac(mac);
+				
+				deviceList.add(device);
 			}
 		}else {
 			    WebElement pageListDiv = driver.findElement(By.id("dhcpLeaseTblpagIngList"));
@@ -102,7 +105,6 @@ public class MyTest2 {
 				for (WebElement span : spanList) {
 					i++;
 					List<WebElement> trList = table.findElements(By.xpath("//tbody/tr[@style='display: table-row;']"));
-					System.out.println("共有"+ (trList.size() - spanList.size())+"台设备在线");
 					System.out.println();
 					System.out.println("第"+i+"页共"+trList.size()+"台设备");
 					for (WebElement tr : trList) {
